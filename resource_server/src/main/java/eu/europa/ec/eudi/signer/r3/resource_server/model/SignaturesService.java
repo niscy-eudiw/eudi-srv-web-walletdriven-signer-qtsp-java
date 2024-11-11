0fad2be4ff019fd59e59b821659b904de7df2cc5
MarianaFilipa <marianarodrigues.mr116@gmail.com>
2024-11-11 18:34:53 +0000

    Update README.md, add licenses file and add license to all files

--- resource_server/src/main/java/eu/europa/ec/eudi/signer/r3/resource_server/model/SignaturesService.java
+++ resource_server/src/main/java/eu/europa/ec/eudi/signer/r3/resource_server/model/SignaturesService.java
@@ -1,17 +1,29 @@
+/*
+ Copyright 2024 European Commission
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
 package eu.europa.ec.eudi.signer.r3.resource_server.model;
 
 import eu.europa.ec.eudi.signer.r3.resource_server.model.database.entities.Credentials;
 import eu.europa.ec.eudi.signer.r3.resource_server.model.database.repositories.CredentialsRepository;
 import eu.europa.ec.eudi.signer.r3.resource_server.model.keys.hsm.HsmService;
 import eu.europa.esig.dss.enumerations.*;
-
-import java.io.ByteArrayOutputStream;
-import java.math.BigInteger;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 import org.bouncycastle.asn1.ASN1ObjectIdentifier;
-import org.bouncycastle.asn1.DERSequenceGenerator;
 import org.bouncycastle.operator.DefaultAlgorithmNameFinder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
