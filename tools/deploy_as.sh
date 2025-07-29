afda4fb17bc0d0c7c94299ec6350899b72046e3a
MarianaFilipa <marianarodrigues.mr116@gmail.com>
2025-07-29 15:21:47 +0100

    Update deployment script

--- tools/deploy_as.sh
+++ tools/deploy_as.sh
@@ -1,1 +1,1 @@
-cd ..; mvn clean install; java -jar authorization_server/target/authorization_server-0.0.1-SNAPSHOT.jar \ --spring.config.additional-location=config_files/application-secret.yml
+cd ..; mvn clean install; nohup java -jar authorization_server/target/authorization_server-0.0.1-SNAPSHOT.jar --spring.config.additional-location=config_files/application-secret.yml &
--- tools/deploy_rs.sh
+++ tools/deploy_rs.sh
@@ -1,1 +1,1 @@
-cd ..; java -jar resource_server/target/resource_server-0.0.1-SNAPSHOT.jar
+cd ..; nohup java -jar resource_server/target/resource_server-0.0.1-SNAPSHOT.jar &
