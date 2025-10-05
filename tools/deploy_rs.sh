b46dc0ab9c478ed078f538903d4e3ecb6330a0a0
Mariana Rodrigues <62109899+MarianaFilipa@users.noreply.github.com>
2025-10-05 23:54:15 +0100

    Small update

--- tools/deploy_rs.sh
+++ tools/deploy_rs.sh
@@ -1,2 +1,2 @@
 export SPRING_PROFILES_ACTIVE=dev
-java -jar resource_server/target/resource_server-0.0.1-SNAPSHOT.jar > nohup_RS.out
+java -jar resource_server/target/resource_server-0.0.1-SNAPSHOT.jar
